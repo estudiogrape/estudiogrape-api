7faccc122bb4c9523ff7952ea0e66223bbebaf25
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-24 17:44:50 -0300

    Adicionar GoodSun na API

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { FindContatoController } from "./controllers/FindContatoController";
 import { CreateContatoController } from "./controllers/CreateContatoController";
+import { GoodSunCreateEmail } from "./controllers/GoodSunCreateEmail";
 
 const router = Router();
 const example = {};
@@ -14,5 +15,10 @@ router.post("/contact/create", contactCreate.handle);
 router.get("/", function (req, res) {
     res.json(example);
 });
+
+
+// GOOD SUN
+const goodSunCreateEmail = new GoodSunCreateEmail();
+router.post("/goodsun/email/create", goodSunCreateEmail.handle);
 
 export { router };
